3444ee7fba63ed74e717ee2eab0cd8f73a1c57e6
IN1DM3B_19 <IN1DM3B_19@H015112.NET>
2019-11-05 09:24:26 +0100

    panel empleado

--- src/main/java/vista/controladores/CMenuEmpleados.java
+++ src/main/java/vista/controladores/CMenuEmpleados.java
@@ -1,5 +1,9 @@
 package vista.controladores;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
 import javax.swing.JTabbedPane;
 
 import vista.graficos.GMenuDepartamentos;
@@ -7,8 +11,43 @@ import vista.graficos.GMenuEmpleados;
 
 public class CMenuEmpleados extends GMenuEmpleados {
 
+		
+		
+	
 	public CMenuEmpleados(JTabbedPane tabbedPane) {
 		super(tabbedPane);
+		AnadirListeners();
+		
+		
 	}
 
+	private void AnadirListeners() {
+		nuevo.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				
+				
+				
+			}
+		});	
+		buscar.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				String valorCombo = comboBox.getSelectedItem().toString();
+				String ValorBuscar = textField.getText().toString();
+				textPane.setText(valorCombo +" "+ ValorBuscar);
+				
+				
+			}
+		});	
+	}
+	
+		
+		public void MostrarMensaje(String mensaje)
+		{
+			System.out.println("[Info](Menu) " + mensaje);
+		}
 }
+	
--- src/main/java/vista/graficos/GMenuEmpleados.java
+++ src/main/java/vista/graficos/GMenuEmpleados.java
@@ -10,23 +10,72 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTabbedPane;
 import javax.swing.JTextField;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JList;
+import javax.swing.JComboBox;
+import javax.swing.DefaultComboBoxModel;
+import javax.swing.JTextPane;
 
 public class GMenuEmpleados extends GPestana {
+	public JTextField textField;
+	public JButton nuevo;
+	public JComboBox comboBox ;
+	public JButton buscar ;
+	public JTextPane textPane ;
+	
 
 
 
 	public GMenuEmpleados(JTabbedPane tabbedPane)
 	{
 		super(tabbedPane, "Empleados", new ImageIcon(GMenu.class.getResource("/res/empleados.jpg")));
+		 JPanel panel = new JPanel();
+		 
+		 JPanel panel_1 = new JPanel();
+		 GroupLayout gl_panel = new GroupLayout(panel);
+		 gl_panel.setHorizontalGroup(
+		 	gl_panel.createParallelGroup(Alignment.LEADING)
+		 		.addGroup(gl_panel.createSequentialGroup()
+		 			.addContainerGap()
+		 			.addComponent(panel_1, GroupLayout.PREFERRED_SIZE, 427, GroupLayout.PREFERRED_SIZE)
+		 			.addContainerGap(89, Short.MAX_VALUE))
+		 );
+		 gl_panel.setVerticalGroup(
+		 	gl_panel.createParallelGroup(Alignment.LEADING)
+		 		.addGroup(gl_panel.createSequentialGroup()
+		 			.addContainerGap()
+		 			.addComponent(panel_1, GroupLayout.PREFERRED_SIZE, 254, GroupLayout.PREFERRED_SIZE)
+		 			.addContainerGap(103, Short.MAX_VALUE))
+		 );
+		 panel_1.setLayout(null);
+		 
+		  nuevo = new JButton("Nuevo Empleado");
+		 nuevo.setBounds(132, 11, 146, 23);
+		 panel_1.add(nuevo);
+		 
+		  comboBox = new JComboBox();
+		 comboBox.setModel(new DefaultComboBoxModel(new String[] {"C\u00F3digo", "Nombre"}));
+		 comboBox.setBounds(37, 63, 64, 20);
+		 panel_1.add(comboBox);
+		 
+		 textField = new JTextField();
+		 textField.setBounds(132, 63, 146, 20);
+		 panel_1.add(textField);
+		 textField.setColumns(10);
+		 
+		  buscar = new JButton("Buscar");
+		 buscar.addActionListener(new ActionListener() {
+		 	public void actionPerformed(ActionEvent arg0) {
+		 	}
+		 });
+		 buscar.setBounds(288, 62, 89, 23);
+		 panel_1.add(buscar);
+		 
+		  textPane = new JTextPane();
+		 textPane.setBounds(65, 109, 292, 110);
+		 panel_1.add(textPane);
+		 panel.setLayout(gl_panel);
 		
-		JButton btnNuevoEmpleado = new JButton("Nuevo empleado");
-		panel.add(btnNuevoEmpleado);
-			
-		JTextField textField = new JTextField();
-		textField.setColumns(10);
-		
-		JLabel lblBuscar = new JLabel("Buscar");
-		panel.add(lblBuscar);
 	}
-	
 }
